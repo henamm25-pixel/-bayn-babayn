1ef339547a6f61f455ad0590d061a06fe44e69e5
henamm25-pixel <henamm25@gmail.com>
2026-07-28 17:58:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const hour = new Date().getHours();
+
+    if (hour >= 18 || hour < 6) {
+        document.body.style.background =
+            "linear-gradient(to bottom, #0f172a, #334155)";
+        document.body.style.color = "#ffffff";
+    }
+
+});
